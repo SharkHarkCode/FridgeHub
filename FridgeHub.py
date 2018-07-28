30beae2a172c8fe1ccf051c3333029e292a00aeb
SharkHarkCode <coolgaming2016@outlook.com>
2018-07-28 08:33:44 -0400

    Added food counter

--- FridgeHub.py
+++ FridgeHub.py
@@ -1,9 +1,11 @@
 import time
 import turtle
 import math
+FridgeFoodAmount = 0
 FridgeEmpty = input("Is your Firdge Empty?")
 FridgeEmpty_Check = 0
 if  FridgeEmpty = "yes":
     print("Saved")
 else:
     FridgeEmpty_Check = 1
+    FridgeFoodAmount = input("How much food do you have")
