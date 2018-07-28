1b3e68543f133719b576306cd786f97625e313de
SharkHarkCode <coolgaming2016@outlook.com>
2018-07-28 08:04:25 -0400

    Added 'Is fridge empty inout command

---
+++ FridgeHub.py
@@ -0,0 +1,10 @@
+import time
+import turtle
+import math
+FridgeEmpty = input("Is your Firdge Empty?")
+FridgeEmpty_Check = 0
+if  FridgeEmpty = "Yes":
+    print("Saved")
+else:
+    FridgeEmpty_Check = 1
+    
