c94dd3da9d0442fdb0de83ace51644852f07bc84
SharkHarkCode <coolgaming2016@outlook.com>
2018-07-28 08:36:54 -0400

    Added Warning

--- FridgeHub.py
+++ FridgeHub.py
@@ -1,6 +1,7 @@
 import time
 import turtle
 import math
+print("When asked questions MAKE IT LOWERCASE, if it the entire answer is not lowercase the app will stop working.")
 FridgeFoodAmount = 0
 FridgeEmpty = input("Is your Firdge Empty?")
 FridgeEmpty_Check = 0
